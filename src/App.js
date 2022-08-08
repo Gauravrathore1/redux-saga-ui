6e16f60e3147863e155afea5e643d4cdff217414
Gaurav Rathore <grathore@ebizneeds.local>
2022-08-08 18:52:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Appbar from './Appbar';
+import Demo from './Demo';
+
+function App() {
+  return (
+    <div className="App">
+       <Appbar/>
+    
+    
+    </div>
+  );
+}
+
+export default App;
